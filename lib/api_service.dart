6dfb361cbf7b7f097dbc5802ac450f3f112a30ef
bk1784 <futonabuu456@gmail.com>
2024-12-23 23:08:38 +0700

    crypto

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -3,18 +3,18 @@ import 'dart:convert';
 
 import 'package:shared_preferences/shared_preferences.dart';
 
-class ApiService {
-  Future<List<dynamic>> fetchPhotos() async {
+class CryptoData {
+  Future<List<dynamic>> fetchCrypto() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    String? chacheData = prefs.getString('photos');
+    String? chacheData = prefs.getString('crypto');
 
     if (chacheData != null) {
       return json.decode(chacheData);
     } else {
       final response = await http.get(Uri.parse(
-          'https://jsonplaceholder.typicode.com/photos')); //jika datanya konek maka dimasukkan await
+          'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')); //jika datanya konek maka dimasukkan await
       if (response.statusCode == 200) {
-        prefs.setString('photos', response.body);
+        prefs.setString('crypto', response.body);
         return json.decode(response.body);
       } else {
         throw Exception('gagal sinkron');
--- lib/main.dart
+++ lib/main.dart
@@ -11,105 +11,130 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Bagus Kurniaawan',
-      
-      home: PhotoScreen(),
+      debugShowCheckedModeBanner: false,
+      title: 'Crypto Prices',
+      home: CryptoScreen(),
     );
   }
 }
 
-class PhotoScreen extends StatelessWidget {
-  final ApiService apiService = ApiService();
+class CryptoScreen extends StatefulWidget {
+   const CryptoScreen({super.key});
+
+  @override
+  _CryptoScreenState createState() => _CryptoScreenState();
+}
+
+class _CryptoScreenState extends State<CryptoScreen> {
+  late Future<List<dynamic>> cryptoDataFuture;
+  List<dynamic>? filteredData;
+
+  @override
+  void initState() {
+    super.initState();
+    cryptoDataFuture = CryptoData().fetchCrypto();
+  }
+
+  void filterCrypto(String query, List<dynamic> data) {
+    setState(() {
+      if (query.isEmpty) {
+        filteredData = null;
+      } else {
+        filteredData = data.where((crypto) {
+          return crypto['name']
+              .toString()
+              .toLowerCase()
+              .contains(query.toLowerCase());
+        }).toList();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('API-post'),
+        backgroundColor: Colors.black,
+        title: const Text(
+          'Crypto',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
       ),
+      backgroundColor:  Color.fromRGBO(58, 66, 86, 1.0),
       body: FutureBuilder<List<dynamic>>(
-          future: apiService
-              .fetchPhotos(), //untuk menarik data pada fetchPost yang ada pada main.dart
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            } else if (snapshot.hasError) {
-              return Center(
-                child: Text('Error: ${snapshot.error}'),
-              );
-            } else {
-              return GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-              ),
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  elevation: 5,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Image.network(
-                        snapshot.data![index]['thumbnailUrl'],
-                        fit: BoxFit.cover,
-                        height: 120,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          snapshot.data![index]['title'],
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              },
+        future:
+            cryptoDataFuture, //untuk menarik data pada fetchPost yang ada pada main.dart
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
             );
-            }
-          }),
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          } else {
+            List<dynamic> data = filteredData ?? snapshot.data!;
+            data.sort(
+                (a, b) => b['current_price'].compareTo(a['current_price']));
+            return Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      labelStyle: TextStyle(color: Colors.white),
+                      labelText: 'search',
+                      border: OutlineInputBorder(
+                       
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                        
+                      )
+                    ),
+                    onChanged: (value) {
+                      filterCrypto(value, snapshot.data!);
+                    },
+                  ),
+                ),
+                Expanded(
+                    child: ListView.builder(
+                        itemCount: data.length,
+                        itemBuilder: (context, index) {
+                          final crypto = data[index];
+                          return Card(
+                            color: Colors.black,
+                            margin: EdgeInsets.symmetric(
+                                vertical: 8, horizontal: 16),
+                            child: ListTile(
+                              leading: Image.network(
+                                crypto['image'],
+                                width: 50,
+                                height: 50,
+                              ),
+                              title: Text(crypto['name'], style: TextStyle(color: Colors.white),),
+                              subtitle:
+                                  Text('Price: \$${crypto['current_price']}', style: TextStyle(color: Colors.grey[600]),),
+                              trailing: Text(
+                                'Change: ${crypto['price_change_percentage_24h'].toStringAsFixed(2)}%',
+                                style: TextStyle(
+                                  color:
+                                      crypto['price_change_percentage_24h'] >= 0
+                                          ? Colors.green
+                                          : Colors.red,
+                                ),
+                              ),
+                            ),
+                          );
+                        }))
+              ],
+            );
+          }
+        },
+      ),
     );
   }
-
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     appBar: AppBar(
-  //       title: Text('API-post'),
-  //     ),
-  //     body: FutureBuilder<List<dynamic>>(
-  //         future: apiService.fetchPhotos(), //untuk menarik data pada fetchPost yang ada pada main.dart
-  //         builder: (context, snapshot) {
-  //           if (snapshot.hasError) {
-  //             return Center(
-  //               child: CircularProgressIndicator(),
-  //             );
-  //           } else if (snapshot.hasError) {
-  //             return Center(
-  //               child: Text('Error: ${snapshot.error}'),
-  //             );
-  //           } else {
-  //             return ListView.builder(
-  //                 itemCount: snapshot
-  //                     .data!.length, // menghitung data yang ada pada list
-  //                 itemBuilder: (context, index) {
-  //                   // membuat ketika data dipilih sesuai dengan index
-  //                   return ListTile(
-  //                     title: Text(snapshot.data![index]
-  //                         ['title']), // mengambil data yang ada pada title json
-  //                     subtitle: Text(snapshot.data![index]
-  //                         ['body']), //mengambil data yang ada pada body json
-  //                   );
-  //                 });
-  //           }
-  //         }),
-  //   );
-  // }
 }
